be91adeb585479cd09ffb51b4c51a0fd11f95d4b
Ankith Sakala <asakala@ncsu.edu>
2018-12-14 23:09:43 -0500

    Added command line output to the runner

--- src/bot.rb
+++ src/bot.rb
@@ -25,6 +25,9 @@ module Bot
     exit(false)
   end
 
+  puts '==CLUSTER: A Shoddy Modular Ruby Bot Framework=='
+  puts 'Initializing the bot object...'
+
   # Creates the bot object. This is a constant in order to make it usable by crystals
   BOT = Discordrb::Commands::CommandBot.new(
     client_id:    config.id,
@@ -39,22 +42,31 @@ module Bot
   # Sets bot's playing game
   BOT.ready { BOT.game = config.game.to_s }
 
+  puts 'Done.'
+
   # Full path string for the crystal data folder (data in parent)
   DATA_PATH = File.expand_path('../data')
 
+  puts 'Loading additional libraries...'
+
   # Loads files from lib directory in parent
-  Dir['../lib/*.rb'].each { |l| load l }
+  Dir['../lib/*.rb'].each do |path| 
+    load path
+    puts "+ Loaded file #{path[3..-1]}"
+  end
+  
+  puts 'Done.'
 
   # Loads a crystal from the given file and includes the module into the bot's container.
   # 
   # @param file [File] the file to load the crystal from. Filename must be the crystal 
   #   name in snake case, or this will not work! (The crystal template generator 
   #   will do this automatically.)
-  def self.load_crystal(file)
-    module_name = File.basename(file.path, '.*').split('_').map(&:capitalize).join
-    load file
+  def self.load_crystal(file_path)
+    module_name = File.basename(file_path, '.*').split('_').map(&:capitalize).join
+    load file_path
     BOT.include! self.const_get(module_name)
-    puts "Loaded crystal #{module_name}"
+    puts "+ Loaded crystal #{module_name}"
   end
 
   # Loads crystals depending on command line flags. Usage as follows:
@@ -63,15 +75,22 @@ module Bot
   # --all, -a: Loads all crystals from both crystals/main and crystals/dev
   # NOTE: Multiple flags are not supported (or needed); only the first argument is read.
   if !ARGV[0] || %w(--main -m --all -a).include?(ARGV[0])
+    puts 'Loading main crystals...'
     Dir['crystals/main/*.rb'].each do |file|
       load_crystal(file)
     end
+    puts 'Done.'
   end
   if %w(--development -d --all -a).include?(ARGV[0])
+    puts 'Loading dev crystals'
     Dir['crystals/dev/*.rb'].each do |file|
       load_crystal(file)
     end
+    puts 'Done.'
   end
+
+  puts 'Starting bot...'
+  BOT.ready { puts 'Bot started!' }
 
   # After loading all desired crystals, runs the bot
   BOT.run
