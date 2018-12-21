cbadb13e791d740a125bb8bdbe6a72a41ff163b8
Ankith Sakala <asakala@ncsu.edu>
2018-12-20 19:26:23 -0600

    Added rake todo to run.rb

--- run.rb
+++ run.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# TODO: Convert this into a Rakefile; I have no idea how rake works but I need to learn
+
 system("cd src; bundle install")
 
 system("cd src; ruby bot.rb #{ARGV.join(' ')}")
